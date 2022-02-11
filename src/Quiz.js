5b87d028edff9fb3218bef0a08c3fedf99eb344d
stevene-payroc <steven.eccles@payroc.com>
2022-02-11 21:04:58 +0000

    Resetting the Quiz

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,12 +16,18 @@ class Quiz extends Component {
         })
     }
 
+    handleResetClick() {
+        this.setState((state) => {
+            return {quiz_position: 1}
+        })
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 == quizData.quiz_questions.length
         if (isQuizEnd) {
             return (
                 <div>
-                    <QuizEnd />
+                    <QuizEnd resetClickHandler={this.handleResetClick.bind(this)}/>
                 </div>
             )
         }
